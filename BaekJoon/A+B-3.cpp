2b153bdef8486921871be794e2fe293d410d3052
qortjddbs(notebook) <beak980513@naver.com>
2026-02-21 23:22:39 +0900

    .

--- BaekJoon/A+B-3.cpp
+++ BaekJoon/A+B-3.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int AddNum(int a, int b) {
-	return a + b;
-}
-
 int main(void) {
 	int testNum = -1;
 	cin >> testNum;
 	int a = 0, b = 0;
+	new int[testNum];
 
 	for (int i = 0; i < testNum; ++i) {
 		cin >> a;
 		cin >> b;
 	}
 
-	AddNum(a, b);
-
 	return 0;
-}
+}-
