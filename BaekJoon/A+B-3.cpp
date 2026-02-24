66217a20a102af3c2ba4c53b6ec24f2671a8abc5
qortjddbs(notebook) <beak980513@naver.com>
2026-02-24 18:33:49 +0900

    문제 하나 완.

--- BaekJoon/A+B-3.cpp
+++ BaekJoon/A+B-3.cpp
@@ -5,12 +5,17 @@ int main(void) {
 	int testNum = -1;
 	cin >> testNum;
 	int a = 0, b = 0;
-	new int[testNum];
+	int *arr = new int[testNum];
 
 	for (int i = 0; i < testNum; ++i) {
 		cin >> a;
 		cin >> b;
+		arr[i] = a + b;
+	}
+
+	for (int i = 0; i < testNum; ++i) {
+		cout << arr[i] << endl;
 	}
 
 	return 0;
-}-
+}
---
+++ BaekJoon/하노이 탑.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	
+	move(num);
+}
+
+int move(int num) {
+	
+}
